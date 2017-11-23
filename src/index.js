cd70753d9144a3b8cb5ae8ddb8874b3b34709b31
Chezhiyan Ramalingam <cramalingam@zokets.com>
2017-11-22 17:05:21 -0800

    jest added

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by cramalingam on 11/22/17.
+ */
+export const sensitiveWords =  (content, words) => {
+    return content.replace(
+        new RegExp(words.join('|'), 'ig'),
+        '***'
+    )
+}
---
+++ src/index.test.js
@@ -0,0 +1,19 @@
+/**
+ * Created by cramalingam on 11/22/17.
+ */
+import {sensitiveWords} from "."
+console.log({sensitiveWords})
+
+test( 'replaces the blacklisted word with astricks', () => {
+    expect(sensitiveWords(
+        'The name os NX will be Nintendo Switch',
+        ['switch']
+    )).toBe('The name os NX will be Nintendo ***')
+})
+
+test( 'replaces multiple instances of the blacklisted word with astricks', () => {
+    expect(sensitiveWords(
+        'The name os NX will be Nintendo Switch. The Switch is awesome',
+        ['switch']
+    )).toBe('The name os NX will be Nintendo ***. The *** is awesome')
+})
